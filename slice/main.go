1b56df9e5c5ad8d880e48cc720d97024966a1b3e
xonal <xonalluck@gmail.com>
2021-12-01 09:15:03 +0700

    initial commit

--- slice/main.go
+++ slice/main.go
@@ -2,33 +2,33 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	// initializeSlice()
 	// lenSclice()
-	lenCap()
+	// lenCap()
 }
 
-func initializeSlice(){
+func initializeSlice() {
 	var fruits = []string{"apple", "mango", "orange", "pineapple"}
 
 	fmt.Println(fruits[0])
 }
 
-func sliceLen(){
+func sliceLen() {
 	var fruits = []string{"apple", "mango", "orange", "pineapple"}
 	fmt.Println(len(fruits)) //len() digunakan untuk menghitung jumlah elemen slice yang ada
 }
 
-func sliceCap(){
+func sliceCap() {
 	var fruits = []string{"apple", "mango", "orange", "pineapple"}
 	var fruit = fruits[1:4]
 	fmt.Println(cap(fruit)) //cap() digunakan untuk menghitung lebar atau kapasitas maksimum slice
 
 }
 
-func sliceAppend(){
+func sliceAppend() {
 	var fruits = []string{"apple", "banana", "orange", "pineapple"}
-	
+
 	addFruits := append(fruits, "papaya") //append() digunakan untuk menambah elemen pada slice dibagian akhir
 
 	fmt.Println(addFruits)

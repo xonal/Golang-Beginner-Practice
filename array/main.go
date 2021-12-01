5aa4fc7b729972396e2aedf7b3b4c6468f29c610
xonal <xonalluck@gmail.com>
2021-12-01 14:27:44 +0700

    Edit 01/12/2021

--- array/main.go
+++ array/main.go
@@ -6,17 +6,17 @@ import (
 
 func main() {
 
-	defaultArray() //1
+	// defaultArray() //1
 
-	verticalHorizontalArray() //2
+	// verticalHorizontalArray() //2
 
-	dynamicArray() //3
+	// dynamicArray() //3
 
-	rangeArray() //4
+	// rangeArray() //4
 
 	multidimensionalArray() //5
 
-	makeArray() //6
+	// makeArray() //6
 }
 
 func defaultArray() { //#1
@@ -47,7 +47,7 @@ func verticalHorizontalArray() { //#2
 
 }
 
-func dynamicArray() {
+func dynamicArray() { //#3
 	//Dynamic Array adalah array yang nilainya tidak dibatasi jumlah maksimum nilainya
 	var numbers = [...]int{1, 2, 4, 6} //dynamic array ditulis menggunakan [...]
 
@@ -56,7 +56,7 @@ func dynamicArray() {
 	}
 }
 
-func rangeArray() {
+func rangeArray() { //#4
 	var names = [...]string{"Abdul", "Bisma", "Celine", "Desi"}
 	for i, name := range names { //range adalah solusi perulangan untuk array
 		// i(underscore) adalah index array (0, 1, 2, 3) dan name adalah index hasil dari array names ("Abdul", "Bisma", "Celine", "Desi")
@@ -64,12 +64,24 @@ func rangeArray() {
 	}
 }
 
-func multidimensionalArray() {
+func multidimensionalArray() { //#5
+	//MultidimensionalArray adalah array 2 dimensi
 	numbers := [2][4]int{{3, 2, 3, 4}, {3, 4, 5, 6}}
-	fmt.Println("Numbers1:", numbers)
+
+	for i := 0; i < len(numbers); i++ { //Melakukan pengulangan untuk menampilkan masing-masing dari baris (looping baris)
+		fmt.Println("Numbers: ", numbers[i])
+	}
+
+	for i := 0; i < len(numbers); i++ { //Melakukan pengulangan baris
+		for j := 0; j < len(numbers[i]); j++ { //Melakukan pengulangan kolom
+			fmt.Println("Numbers: ", numbers[i][j])
+		}
+
+	}
+	// fmt.Println("Numbers1:", numbers)
 }
 
-func makeArray() {
+func makeArray() { //#6
 	fruits := make([]string, 2) //fungsi make digunakan untuk membuat dynamic array
 	fruits[0] = "apple"
 	fruits[1] = "mango"
